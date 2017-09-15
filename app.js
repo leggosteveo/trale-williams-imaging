7782d91e66aa84a1fc03dffbcd397a4d8a666b7e
Stephen Tyler <Stephenjovon@gmail.com>
2017-09-14 20:37:47 -0400

    Demo

--- app.js
+++ app.js
@@ -10,3 +10,9 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use('/fonts', express.static(__dirname + '/fonts'));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
