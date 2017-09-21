717df01a3b11df3f45641e1a35d269810609b1f4
Stephen Tyler <Stephenjovon@gmail.com>
2017-09-21 16:33:29 -0400

    Demo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 // Define the port to run on
 app.set('port', (process.env.PORT || 5000));
 
@@ -7,7 +8,7 @@ app.set('port', (process.env.PORT || 5000));
 
 
 // Set static directory before defining routes
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(path.join(__dirname, + 'public')));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use('/fonts', express.static(__dirname + '/fonts'));
 
